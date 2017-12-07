16709f333c4c626a5ea65283f991c934aaa8e1b0
taylorrc <taylorrc@rose-hulman.edu>
2017-12-07 11:30:08 -0500

    say my name

--- taylorrc/m1_say_your_name.py
+++ taylorrc/m1_say_your_name.py
@@ -17,3 +17,16 @@ Authors: Dave Fisher and PUT_YOUR_NAME_HERE.
 #   Feel free to run it, change it, and run it again with any message you like.
 #   This program is simple, but if you can do this, then you have the tools to do FAR more.
 # ------------------------------------------------------------------
+
+import ev3dev.ev3 as ev3
+
+
+def main():
+    """ Creates the LargeMotor objects and passes them to other functions. """
+    print("--------------------------------------------")
+    print("  Say your name")
+    print("--------------------------------------------")
+    ev3.Sound.speak("Ryan Taylor").wait()
+
+
+main()
